1a154a6f786ec00569dbc48d604ddf30f55dd755
yunyinghuang <131827216+yunyinghuang@users.noreply.github.com>
2023-06-06 21:59:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const dino = document.getElementById('dino');
+const rock = document.getElementById('rock');
+const score = document.getElementById('score');
+
+function jump(){
+	dino.classList.add('jump-animation');
+	setTimeout(() => {
+		dino.classList.remove('jump-animation');
+		} ,500);	
+}
+
+document.addEventListener('keypress', () => {
+	if (!dino.classList.contains('jump-animation')){
+		jump();
+	}
+});
+
+setInterval(() =>{
+	score.innerText++;
+	const dinoTop = parseInt(window.getComputedStyle(dino)
+		.getPropertyValue('top'));
+	const rockLeft = parseInt(window.getComputedStyle(rock)
+		.getPropertyValue('left'));
+
+	if (rockLeft < 0) {
+		rock.style.display = 'none';
+	}else {
+		rock.style.display ='';
+	}
+	if(rockLeft < 50 && rockLeft > 0 && dinoTop > 150) {
+		alert("You got a score of: " + score.innerText +
+			"\n\nPlay again?");
+		location.reload();
+	}
+}, 50);
+
